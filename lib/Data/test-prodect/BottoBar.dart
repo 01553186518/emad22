b7a88ebd6411da7d66c5ce7da2a46892a87b5d13
eng.emad sayed <emadsayed117@gamil.com>
2023-05-17 20:25:55 +0300

    the new code cart

---
+++ lib/Data/test-prodect/BottoBar.dart
@@ -0,0 +1,32 @@
+// import 'package:flutter/cupertino.dart';
+// import 'package:flutter/material.dart';
+//
+// class BottamBar extends StatelessWidget {
+//
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return BottomAppBar(child: Container(
+//       height: 70,
+//       padding: EdgeInsets.symmetric(horizontal: 20),
+//       decoration: BoxDecoration(
+//         color: Colors.white,
+//       ),
+//       child: Row(
+//         // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+//         children: [
+//           Text("\$120",style: TextStyle(
+//               fontSize: 15,
+//               fontWeight: FontWeight.bold,
+//               color: Colors.redAccent
+//           ),
+//           ),
+//           InkWell(
+//             onTap: (){},
+//           )
+//         ],
+//       ),
+//     ),
+//     );
+//   }
+// }
---
+++ lib/widget/popular_order/shrenk_big.dart
@@ -0,0 +1,34 @@
+//
+// import 'package:flutter/animation.dart';
+//
+// class Sneaker {
+//   const Sneaker({
+//     required this.id,
+//     required this.shortName,
+//     required this.name,
+//     required this.brandName,
+//     required this.price,
+//     // required this.assets,
+//     // required this.type,
+//     required this.color,
+//     required this.description,
+//     required this.sizes,
+//   });
+//
+//   final int id;
+//   final String shortName;
+//   final String name;
+//   final String brandName;
+//   final double price;
+//   // final List<Asset> assets;
+//
+//   final Color color;
+//   final String description;
+//   final List<double> sizes;
+//
+//   // String get priceAsCurrency => price.toCurrency();
+//
+//   String get image => assets[0].path;
+//
+//   // Color get estimatedColor => color.estimate();
+// }
